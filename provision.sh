c2124cd3640487a7d964312b2a6149fac09db25b
jkr <joakim.karlsson@lundalogik.se>
2017-05-06 11:11:47 +0200

    Skip salt and install Gnome 3 directly

--- provision.sh
+++ provision.sh
@@ -1,1 +1,5 @@
-sudo apt install -y salt-minion
+# Install desktop environment
+sudo add-apt-repository ppa:gnome3-team/gnome3
+sudo apt update
+sudo apt install -y xorg gdm ubuntu-desktop menu firefox gksu gnome-shell --no-install-recommends
+sudo apt upgrade -y

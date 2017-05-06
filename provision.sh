d05f8928c3b27288e1da1e86cff8fea375556a2f
jkr <joakim.karlsson@lundalogik.se>
2017-05-06 14:41:57 +0200

    Back to xubuntu

--- provision.sh
+++ provision.sh
@@ -1,11 +1,12 @@
 # Install desktop environment
 sudo apt install software-properties-common -y
+
+# Needed for libxcb-xrm-dev, needed by i3wm
 sudo add-apt-repository ppa:aguignard/ppa -y
-sudo apt-add-repository ppa:ubuntu-mate-dev/xenial-mate -y
 sudo apt update
 
 # Install Xfce4
-sudo apt install mate -y
+sudo apt install xubuntu-desktop --no-install-recommends -y
 
 # Dependencies for i3-gaps
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y

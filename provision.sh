0fc01e25d906fe0cba9b7080fca33ec0f2687141
jkr <joakim.karlsson@lundalogik.se>
2017-05-06 12:10:06 +0200

    Minimal Xfce4

--- provision.sh
+++ provision.sh
@@ -3,11 +3,13 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:aguignard/ppa -y
 sudo apt update
 
+# Install Xfce4
+sudo apt install xfce4 -y
+
 # Dependencies for i3-gaps
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y
 
 # Get i3-gaps
-sudo apt install git -y
 git clone https://www.github.com/Airblader/i3 ~/i3-gaps
 
 pushd ~/i3-gaps

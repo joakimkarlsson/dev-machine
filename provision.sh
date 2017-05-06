93319705941304b873bbe00db876d7de71bdb22e
jkr <joakim.karlsson@lundalogik.se>
2017-05-06 13:26:16 +0200

    Trying mate...

--- provision.sh
+++ provision.sh
@@ -1,10 +1,11 @@
 # Install desktop environment
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:aguignard/ppa -y
+sudo apt-add-repository ppa:ubuntu-mate-dev/xenial-mate -y
 sudo apt update
 
 # Install Xfce4
-sudo apt install xfce4 lightdm -y
+sudo apt install mate -y
 
 # Dependencies for i3-gaps
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y
@@ -13,7 +14,7 @@ sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-de
 if [[ ! -d ~/i3-gaps ]]; then
     git clone https://www.github.com/Airblader/i3 ~/i3-gaps
 else
-    pusd ~/i3-gaps
+    pushd ~/i3-gaps
     git pull
     popd
 fi

69bffe6b086beaf7fb53c9f18e84a631659e6318
jkr <joakim.karlsson@lundalogik.se>
2017-05-06 17:17:24 +0200

    Polybar build

--- provision.sh
+++ provision.sh
@@ -1,16 +1,16 @@
 # Install desktop environment
-sudo apt install software-properties-common -y
+apt install software-properties-common -y
 
 # Needed for libxcb-xrm-dev, needed by i3wm
-sudo add-apt-repository ppa:aguignard/ppa -y
-sudo apt update
+add-apt-repository ppa:aguignard/ppa -y
+apt update
 
 # Install Xfce4
-sudo apt install xubuntu-desktop gnome-terminal menu --no-install-recommends -y
-sudo apt install polybar gnome-terminal xfwm4-themes xfce4-terminal --no-install-recommends -y
+apt install xubuntu-desktop gnome-terminal menu --no-install-recommends -y
+apt install gnome-terminal xfwm4-themes xfce4-terminal --no-install-recommends -y
 
 # Dependencies for i3-gaps
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y
+apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y
 
 # Get i3-gaps
 if [[ ! -d ~/i3-gaps ]]; then
@@ -28,6 +28,23 @@ mkdir -p build && cd build/
 
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
-sudo make install
+make install
 
+popd
+
+# polybar
+apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev -y
+
+if [[ ! -d ~/polybar ]]; then
+    git clone --branch 3.0.5 --recursive https://github.com/jaagr/polybar
+else
+    pushd polybar
+    git pull
+    popd
+fi
+
+mkdir polybar/build
+pushd polybar/build
+cmake ..
+make install
 popd

4784c742d7598ea195bdf1900972611f4b2bc342
jkr <joakim.karlsson@lundalogik.se>
2017-05-07 07:58:37 +0200

    Fix copying and don't sudo everything

--- provision.sh
+++ provision.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # Install desktop environment
-apt install software-properties-common -y
+sudo apt install software-properties-common -y
 
 # Needed for libxcb-xrm-dev, needed by i3wm
 add-apt-repository ppa:aguignard/ppa -y
-apt update
+sudo apt update
 
 # Install Xfce4
-apt install xubuntu-desktop gnome-terminal menu --no-install-recommends -y
-apt install gnome-terminal xfwm4-themes xfce4-terminal --no-install-recommends -y
+sudo apt install xubuntu-desktop gnome-terminal menu --no-install-recommends -y
+sudo apt install gnome-terminal xfwm4-themes xfce4-terminal --no-install-recommends -y
 
 # Dependencies for i3-gaps
-apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm-dev libxcb-xrm-dev -y
 
 # Get i3-gaps
 if [[ ! -d ~/i3-gaps ]]; then
@@ -30,12 +30,12 @@ mkdir -p build && cd build/
 
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
-make install
+sudo make install
 
 popd
 
 # polybar
-apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev -y
+sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev -y
 
 if [[ ! -d "~/polybar" ]]; then
     git clone --branch 3.0.5 --recursive https://github.com/jaagr/polybar ~/polybar
@@ -48,13 +48,13 @@ fi
 mkdir ~/polybar/build
 pushd ~/polybar/build
 cmake ..
-make install
+sudo make install
 popd
 
 mkdir -p ~/.config/polybar
-cp -R ~/machines/home/.config/polybar ~/.config/polybark
+cp -R ~/machines/home/.config/polybar ~/.config/polybar
 
 mkdir -p ~/.config/i3
 cp -R ~/machines/home/.config/i3 ~/.config/i3
 
-cp ~/machines/home/* ~
+cp ~/machines/home/.xinitrc ~
